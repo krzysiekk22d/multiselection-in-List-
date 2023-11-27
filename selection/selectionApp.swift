4ecb48731946493e1e8eddda70cfc1f46f043247
krzysiekk22d <klas.club@gmail.com>
2023-11-27 23:52:26 +0100

    Initial Commit

---
+++ selection/selectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  selectionApp.swift
+//  selection
+//
+//  Created by Krzysztof Czura on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct selectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
